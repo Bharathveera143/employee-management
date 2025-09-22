294ff287c85e9f391975d032993da11e32d50a96
Bharath S <bharathveera143@gmail.com>
2025-09-22 19:03:44 +0530

    Swagger Updted

--- employee-management/src/app.ts
+++ employee-management/src/app.ts
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import employeeRoutes from "./routes/employeeRoutes";
+import { setupSwagger } from "./config/swagger.config";
 
 
 
 dotenv.config();
+
 const app = express();
+
 app.use(express.json());
+
+
+// Swagger Documentation
+
+setupSwagger(app);
+
+//Routes
+
 app.use('/employees',employeeRoutes);
 
 export default app;
--- employee-management/src/routes/employeeRoutes.ts
+++ employee-management/src/routes/employeeRoutes.ts
@@ -1,14 +1,167 @@
-import{Router} from "express";
+import { Router } from "express";
 import { EmployeeController } from "../controllers/employeeController";
 import { authenticateJWT } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post('/register',EmployeeController.register);
-router.post('/login',EmployeeController.login);
-router.get('/',authenticateJWT,EmployeeController.getAll);
-router.get('/:id',authenticateJWT,EmployeeController.getById);
-router.put('/:id',authenticateJWT,EmployeeController.update);
-router.delete('/:id',authenticateJWT,EmployeeController.delete);
+/**
+ * @swagger
+ * /employees/register:
+ *   post:
+ *     summary: Register a new employee
+ *     tags: [Employees]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               department:
+ *                 type: string
+ *               salary:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Employee registered successfully
+ */
+
+router.post('/register', EmployeeController.register);
+
+
+/**
+ * @swagger
+ * /employees/login:
+ *   post:
+ *     summary: Login an employee
+ *     tags: [Employees]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful, returns JWT
+ *       401:
+ *         description: Invalid credentials
+ */
+
+router.post('/login', EmployeeController.login)
+
+/**
+ * @swagger
+ * /employees:
+ *   get:
+ *     summary: Get all employees
+ *     tags: [Employees]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of employees
+ */
+router.get('/',authenticateJWT, EmployeeController.getAll)
+
+/**
+ * @swagger
+ * /employees/{id}:
+ *   get:
+ *     summary: Get employee by ID
+ *     tags: [Employees]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Employee found
+ *       404:
+ *         description: Employee not found
+ */
+router.get('/:id',authenticateJWT, EmployeeController.getById);
+
+/**
+ * @swagger
+ * /employees/{id}:
+ *   put:
+ *     summary: Update employee
+ *     tags: [Employees]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               department:
+ *                 type: string
+ *               salary:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Employee updated successfully
+ *       404:
+ *         description: Employee not found
+ */
+router.put('/:id', EmployeeController.update)
+
+
+/**
+ * @swagger
+ * /employees/{id}:
+ *   delete:
+ *     summary: Delete employee
+ *     tags: [Employees]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Employee deleted successfully
+ *       404:
+ *         description: Employee not found
+ */
+router.delete('/:id', EmployeeController.delete)
 
 export default router;
